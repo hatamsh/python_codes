7d17736c58d9dbe29948fbf2f30d6a77a10371b7
hatamsh <hatam.shukur@gmail.com>
2017-06-30 12:52:18 +0400

    Code optimization

--- backup_automation/backup_script.py
+++ backup_automation/backup_script.py
@@ -9,21 +9,18 @@ Python script for automating backups of Cisco devices.
 Successfully tested on IOS, IOS-XE, IOS-XR, and ASA software.
 """
 
-# Importing paramiko and time modules
+# Importing necessary modules
 import paramiko
 import time
+import re
 
 # Username and passwords for a backup user
 username = "backup"
 password = "backup"
 
 """
-Device IPs are stored in the list.
-In future new IP can be added to the list and for loop will grab it.
+Device IPs are stored in the config files.
 """
-
-router_IPs = ["192.168.99.254"]
-asa_IPs = ["192.168.99.253"]
 
 # Taking the SSHClient
 ssh_conn_paramiko = paramiko.SSHClient()
@@ -31,21 +28,25 @@ ssh_conn_paramiko = paramiko.SSHClient()
 # Auto accept SSH host keys.
 ssh_conn_paramiko.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-
-# Used to disable paging
+# Used to disable paging on Cisco routers and switches
 def disable_paging_ios_iosxr_iosxe(command="terminal length 0\n", delay=2):
     cli_interaction.send("\n")
     cli_interaction.send(command)
     time.sleep(delay)
 
-
+# Used to disable paging on Cisco ASA
 def disable_paging_asa(command="terminal pager 0\n", delay=2):
     cli_interaction.send("\n")
     cli_interaction.send(command)
     time.sleep(delay)
 
+# Opening file with router IPs
+ios_iosxr_iosxe_IPs = open("ios_iosxr_iosxe_IPs.cfg","r")
 
-for each in router_IPs:
+# Taking config backups
+for each in ios_iosxr_iosxe_IPs:
+    each = re.sub(" .*", "", each)
+    each = each.rstrip()
     backup_file = open("/home/hshukur/Config_Backup/Config_{}.txt".format(each), "w")
     backup_file.write("*" * 32 + "{}".format(each) + "*" * 32 + "\n")
     ssh_conn_paramiko.connect(each, username=username, password=password)
@@ -62,8 +63,16 @@ for each in router_IPs:
     backup_file.close()
     ssh_conn_paramiko.close()
 
+# Closing the file with router IPs
+ios_iosxr_iosxe_IPs.close()
 
+# Opening file with ASA IPs
+asa_IPs = open("asa_IPs.cfg","r")
+
+# Taking config backups
 for each in asa_IPs:
+    each = re.sub(" .*", "", each)
+    each = each.rstrip()
     backup_file = open("/home/hshukur/Config_Backup/Config_{}.txt".format(each), "w")
     backup_file.write("*" * 32 + "{}".format(each) + "*" * 32 + "\n")
     ssh_conn_paramiko.connect(each, username=username, password=password)
